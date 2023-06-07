41d27f28c3d5c799f6fac16cf9ebc30c98a78eb3
unknown <limjohandi@gmail.com>
2023-06-07 22:06:34 +0700

    login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Grid} from '@mui/material'
 import logo_text from '../images/logo_text.svg'
+import {Field, Formik, Form} from 'formik'
+import {Link} from 'react-router-dom'
 const Login = () => {
   return (
     <Grid container component="main" sx={{ height: '100vh', width:"100vw"}}>
@@ -13,16 +15,31 @@ const Login = () => {
                     <img className='item' alt='logo' src={logo_text} style={{width: "20rem"}}/>
                     <br/>
                     <br/>
-                    <p className='bold' >Hi, Welcome Back!</p>
+                    <h2 className='text-secondary'>Hi, Welcome Back!</h2>
                     <br/>
                     <div className='item'>
-                        <p>Username</p>
-                        
-                        <p>error message</p>
+                      <Formik
+                        initialValues={{username:"", password:""}}
+                        onSubmit={()=>{console.log("submit")}}
+                      >
+                        <Form className='flex-container'>
+                          <p className='text-secondary'>Email</p>
+                          <Field className="login-text-field secondary" name="email" type="email" />
+                          <p className='text-secondary'>Email error</p>
+                          <br/>
+                          <p className='text-secondary'>Password</p>
+                          <Field className="login-text-field secondary" name="password" type="password" />
+                          <p className='text-secondary'>Password error</p>
+                          <br/>
+                          <div className='flex-container flex-row' style={{alignItems:"center", gap:"2rem"}}>
+                            <button className='login-btn primary' type="submit">Login</button> 
+                            <Link to={"/"}>Forget Password?</Link>
+                          </div>
+                        </Form>
+                      </Formik>
                     </div>
                 </div>
             </div>
-            
         </Grid>
         <Grid
           item
