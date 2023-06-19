6564e5a4239bdb5eb6f31154b5e870940fba30b2
unknown <limjohandi@gmail.com>
2023-06-20 00:15:47 +0700

    env enc

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json({extended:false}))
 
 app.get("/", (req, res)=>{
-    res.send("hello world")
+    const data = process.env.MONGO
+    res.send(data)
 })
 
-app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/auth', require('./routes/auth'))
 
 app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
